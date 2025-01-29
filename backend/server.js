837737f4913be16625699c537aeec5e4b9a40586
divyesh-vasani <divyeshvasani2091@gmail.com>
2025-01-29 23:15:25 +0530

    created basic route and model for login & signup and link backedn with frontend with basic login form to check.

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,28 @@
-const express = require('express'); // Import express framework
-const mongoose = require('mongoose'); // Import mongoose to connect to MongoDB
-const dotenv = require('dotenv'); // Import dotenv to load environment variables
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoutes = require("./routes/authRoutes");
+const cors = require("cors");
 
-dotenv.config(); // Load the variables from the .env file
+dotenv.config();
 
-const app = express(); // Create an instance of the express application
-app.use(express.json()); // Enable the app to handle incoming JSON data
+const app = express();
+app.use(express.json());
+app.use(cors());  
+app.use("/auth", authRoutes);
 
-// Connect to MongoDB using the URI from the .env file
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('Failed to connect to MongoDB', err));
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Failed to connect to MongoDB", err));
 
-// Example route that responds with 'Hello, Backend!' when accessed
-app.get('/', (req, res) => {
-  res.send('Hello, Backend!');
+app.get("/", (req, res) => {
+  res.send("Hello, Backend!");
 });
 
-// Start the server on port 5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
