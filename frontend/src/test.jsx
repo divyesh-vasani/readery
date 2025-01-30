a9b9d9c4048fa1b2afb1070329d7c0dc784d74c3
divyesh-vasani <divyeshvasani2091@gmail.com>
2025-01-30 23:29:32 +0530

    basic register form & added react-hook-form.

--- frontend/src/test.jsx
+++ frontend/src/test.jsx
@@ -1,40 +1,81 @@
 import React, { useState } from 'react'
 import axios from 'axios';
+import { useForm } from "react-hook-form";
+
 
 const Test = () => {
-    const [mail, setMail] = useState("")
-    const [pass, setPass] = useState("")
+    const { register, handleSubmit } = useForm();
 
-    const loginUser = (e) =>{
+    const loginUser = (data,e) =>{
         e.preventDefault();  // Prevent the form from reloading the page
-    console.log("Form submitted"); 
+    console.log("Form submitted",data); 
 
-        axios.post("http://localhost:5000/auth/login",{
-            email: mail,
-            password: pass
-        })
-        .then((res)=>{
-            alert(res.data.message,"res")
-        },(error)=>{
-            alert(error.response.data.message,"error")
-        })
+        // axios.post("http://localhost:5000/auth/login",{
+        //     email: mail,
+        //     password: pass
+        // })
+        // .then((res)=>{
+        //     alert(res.data.message,"res")
+        // },(error)=>{
+        //     alert(error.response.data.message,"error")
+        // })
+    }
+
+    const registerUser = (e,data) =>{
+        e.preventDefault();  // Prevent the form from reloading the page
+    console.log("Form submitted",data); 
+
+        // axios.post("http://localhost:5000/auth/signup",{
+        //     email: mail,
+        //     password: pass,
+        //     name: name
+        // })
+        // .then((res)=>{
+        //     alert(res,"res")
+        // },(error)=>{
+        //     alert(error,"error")
+        // })
     }
 
     return (
         <div>
-            <form onSubmit={loginUser}>
+            <div>
+                <h1>Login</h1>
+            <form onSubmit={handleSubmit(loginUser)}>
                 <input 
                     type='text' 
                     placeholder='mail' 
-                    onChange={(e) => setMail(e.target.value)} 
+                    {...register("mail")}
                 />
                 <input 
                     type='password' 
                     placeholder='pass' 
-                    onChange={(e) => setPass(e.target.value)} 
+                    {...register("pass")}
                 />
                 <button type='submit'>login</button>
             </form>
+            </div>    
+            {/* <div>
+                <h1>Register</h1>
+                <form onSubmit={handleSubmit(registerUser)}>
+                <input 
+                    type='text' 
+                    placeholder='text' 
+                    {...register("fname")}
+                />
+                <input 
+                    type='text' 
+                    placeholder='mail' 
+                    {...register("mail")}
+                />
+                <input 
+                    type='password' 
+                    placeholder='pass' 
+                    {...register("pass")}
+                />
+                <button type='submit'>Register</button>
+            </form>
+                </div>         */}
         </div>
     )
 }
